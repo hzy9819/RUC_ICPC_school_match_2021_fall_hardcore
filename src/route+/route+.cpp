5db3b7f81a86e9345ac2d44637451b73e366de93
ogi <hzy9819@gmail.com>
2021-12-11 00:58:59 +0800

    add route+ src

--- src/route+/route+.cpp
+++ src/route+/route+.cpp
@@ -180,18 +180,18 @@ bool Hypergraph::build_join_forest(Graph & G) {
 }
 
 void DataLoader(const char * file_name, Hypergraph & G) {
-    FILE * fin = fopen(file_name, "r");
-    fin = stdin;
+    // FILE * fin = fopen(file_name, "r");
+    // fin = stdin;
     size_t n, m, sz, tmp;
-    fscanf(fin, "%lu %lu", &n, &m);
+    fscanf(stdin, "%lu %lu", &n, &m);
     G.set(m, n);
 
     std::vector< std::vector<size_t> > V(n);
 
     for(size_t i = 0; i < m; ++i) {
-        fscanf(fin, "%lu", &sz);
+        fscanf(stdin, "%lu", &sz);
         for(size_t j = 0; j < sz; ++j) {
-            fscanf(fin, "%lu", &tmp);
+            fscanf(stdin, "%lu", &tmp);
             V[tmp].push_back(i);
         }
     }
@@ -204,16 +204,16 @@ int main(int argc, char * argv[]) {
     Graph G;
     DataLoader(argv[1], H);
 
-    FILE * fout = fopen(argv[2], "w");
+    // FILE * fout = fopen(argv[2], "w");
 
     if(H.build_join_forest(G)) {
-        fprintf(fout, "YES\n");
+        fprintf(stdout, "YES\n");
         for(size_t i = 0; i < G.n; ++i)
             for(size_t j = G.lst[i]; j != -1; j = G.e[j].nxt)
                 if(i < G.e[j].to)
-                    fprintf(fout, "%lu %lu\n", i, G.e[j].to);
+                    fprintf(stdout, "%lu %lu\n", i, G.e[j].to);
     }
     else 
-        fprintf(fout, "NO\n");
+        fprintf(stdout, "NO\n");
     return 0;
 }
