78ff71f2bfe5bbf927afbb63272791a882c0677d
yyqchemic <yyqchemic@hotmail.com>
2021-12-11 18:58:12 +0800

    交互

--- src/RPS/std.cpp
+++ src/RPS/std.cpp
@@ -2,70 +2,34 @@
 #include <cstring>
 #include <algorithm>
 #include <iostream>
-#include <time.h>
 using namespace std;
 
-int a[22],b[22],real[22];
+int a[12], b[12];
 
-int i, seed, n;
-int top = 0;
+int main() {
+    int temp;
+    bool anc = 0;
+    for (int T = 1; T <= 1000; ++T) {
+        anc = 0;
+        for (int t = 1; t <= 10; ++t) {
+            cout << "?" << endl;
+            fflush(stdout);
+            cin >> temp;
+            a[t] = temp;
+            b[t] = (a[t] - a[t - 1] + 3) % 3;
+            if (t >= 3 && b[t] != b[t - 1]) {
+                int x = (a[t] + b[t - 1]) % 3;
+                cout << "! " << (x + 1) % 3 << endl;
+                fflush(stdout);
+                anc = 1;
+                break;
+            }
+        }
+        if (!anc) {
+            cout << "! " << (a[10] + b[10] + 1) % 3 << endl;
+            fflush(stdout);
+        }
+    }
 
-int judge()
-{
-	(i += seed) %= n;
-	real[++top] = i;
-	return i % 3;
-}
-
-void init_rps()
-{
-	seed = rand() % n;
-	i = 0;
-}
-
-int main()
-{
-	srand((int)time(0));
-	int temp;
-	bool anc = 0;
-	n = 1000;
-	for(int T = 1; T <= 1; ++T)
-	{
-		anc = 0;
-		init_rps();
-		for(int t = 1; t <= 20; ++t)
-		{
-			/*
-			cout << "?" << endl;
-			fflush(stdout);
-			cin >> temp;
-			*/
-			temp = judge();
-			a[t] = temp;
-			b[t] = (a[t] - a[t-1] + 3) % 3;
-		    if(t >= 3 && b[t] != b[t-1])
-			{
-				int x = (a[t] + b[t-1]) % 3;
-				cout << "! " << (x + 1) % 3 << endl;
-				fflush(stdout);
-				anc = 1;
-				break;
-			}
-		}
-		if(!anc)
-		{
-			cout << "! " << (a[20] + b[20]) % 3 << endl;
-			fflush(stdout);
-		}
-	}
-	
-	cout << judge() << endl;
-	for(int t = 1; t <= 20; ++t) cout << real[t] << " ";
-	cout << endl;
-	for(int t = 1; t <= 20; ++t) cout << a[t] << " ";
-	cout << endl;
-	for(int t = 1; t <= 20; ++t) cout << b[t] << " ";
-	system("pause");
-	
-	return 0;
+    return 0;
 }
